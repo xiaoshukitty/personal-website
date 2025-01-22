1aba742f766d5e811e24d47b55042be74862746a
xiaoshu <2376628285@qq.com>
2025-01-22 22:43:58 +0800

    fix:滚动条样式

--- web-project/src/App.vue
+++ web-project/src/App.vue
@@ -17,18 +17,5 @@ provideImagePreview();
 </template>
 
 <style scoped lang="scss">
-body {
-  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
 
-}
-
-/** 去掉页面滚动条 */
-::-webkit-scrollbar {
-  width: 0 !important;
-}
-
-::-webkit-scrollbar {
-  width: 0 !important;
-  height: 0;
-}
 </style>
