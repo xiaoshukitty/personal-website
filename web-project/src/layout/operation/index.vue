7623dce7394c8bd2e62119fdba5328f486c369ca
xiaoshu <2376628285@qq.com>
2025-01-22 22:40:23 +0800

    feat:主题切换功能

--- web-project/src/layout/operation/index.vue
+++ web-project/src/layout/operation/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+
+const theme = ref('light')
 
 const props = defineProps({
     scrollPosition: {
@@ -13,13 +17,42 @@ const scrollOneScreen = (id: string) => {
     emits('scrollOneScreen', id)
 }
 
+//主题切换
+const themeChange = () => {
+    theme.value = theme.value === 'light' ? 'dark' : 'light'
+    // 将当前主题存储到 localStorage 中，便于页面刷新后记住上次的主题
+    localStorage.setItem('theme', theme.value);
+    document.documentElement.setAttribute('data-theme', theme.value)
+}
+
+onMounted(() => {
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme) {
+        theme.value = savedTheme
+        document.documentElement.setAttribute('data-theme', savedTheme)
+    } else {
+        document.documentElement.setAttribute('data-theme', theme.value)
+    }
+})
+
 </script>
 
 <template>
     <div class="shu-action">
         <div :class="['shu-action-item scroll', { 'scroll-active': props.scrollPosition > 280 }]"
             @click="scrollOneScreen('wrapper')">
-            <SvgIcon name="top" :width="'16px'" :height="'16px'" />
+            <!-- <SvgIcon name="rocket" :width="'30px'" :height="'30px'" /> -->
+            <svg class="scroll-active-icon">
+                <use xlink:href="#icon-rocket"></use>
+            </svg>
+        </div>
+        <div class="shu-action-item" @click="themeChange">
+            <svg :class="['item-icon', { 'item-icon-active': theme === 'light' }]">
+                <use xlink:href="#icon-Sun"></use>
+            </svg>
+            <svg :class="['item-icon', { 'item-icon-active': theme === 'dark' }]">
+                <use xlink:href="#icon-Star-Night"></use>
+            </svg>
         </div>
     </div>
 </template>
@@ -43,6 +76,27 @@ const scrollOneScreen = (id: string) => {
         cursor: pointer;
         margin-top: 15px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1), 0 5px 20px rgba(0, 0, 0, 0.2);
+
+        .scroll-active-icon {
+            width: 28px;
+            height: 28px;
+            fill: var(--theme);
+        }
+
+        .item-icon {
+            position: absolute;
+            width: 25px;
+            height: 25px;
+            fill: var(--theme);
+            transform: scale(0);
+            opacity: 0;
+            transition: transform 0.85s, opacity 0.85s;
+        }
+
+        .item-icon-active {
+            transform: scale(1) !important;
+            opacity: 1 !important;
+        }
     }
 
     .scroll {
